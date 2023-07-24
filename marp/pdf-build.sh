2c083605fe8416ef4d777270d7a4f127b9ae631d
Juan Manuel Dodero <juanma.dodero@uca.es>
2023-07-24 12:36:48 +0200

    generar pdf outline

--- marp/pdf-build.sh
+++ marp/pdf-build.sh
@@ -1,2 +1,2 @@
-marp --allow-local-files --config-file ./marp/marp-engine.js --pdf $1.md -o pdf/$1.pdf
+marp --allow-local-files --config-file ./marp/marp-engine.js --pdf $1.md -o pdf/$1.pdf --pdf-outlines --pdf-outlines.pages=false --pdf-outlines.headings=true
 
